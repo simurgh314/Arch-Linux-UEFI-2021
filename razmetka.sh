1a3b44e97d04f97000101a8ad8cc736dbb8a86b3
Simurgh <62325871+simurgh314@users.noreply.github.com>
2021-03-11 21:45:42 +0500

    Update razmetka.sh

--- razmetka.sh
+++ razmetka.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # предварительно требуется wget и parted
+# не забыть изменить vda на sda
 
 
 (
 	
 unit mib
 mklabel gpt
-mkpart primary 2 258		#1 /dev/sda1 256MB EFI (FAT32)
+mkpart primary 2 258		#1 /dev/vda1 256MB EFI (FAT32)
 set 1 boot on
-mkpart primary 258 514		#2 /dev/sda2 256MB boot (ext2)
+mkpart primary 258 514		#2 /dev/vda2 256MB boot (ext2)
 name 2 boot
-mkpart primary 514 4610		#3 /dev/sda3 4GB (SWAP)
+mkpart primary 514 4610		#3 /dev/vda3 4GB (SWAP)
 name 3 swap
-mkpart primary 4610 -1		#4 /dev/sda4 (ext4)
+mkpart primary 4610 -1		#4 /dev/vda4 (ext4)
 name 4 root
 
-) | parted -a optimal /dev/sda  # выравнивание разделов
+) | parted -a optimal /dev/vda  # выравнивание разделов
