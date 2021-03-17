fe3e88a7ae12b57e3162fe331b08c367c8ad9cfc
Simurgh <62325871+simurgh314@users.noreply.github.com>
2021-03-17 22:59:05 +0500

    Update 6_video.sh

--- 6_video.sh
+++ 6_video.sh
@@ -6,3 +6,5 @@ cd && git clone https://aur.archlinux.org/nvidia-390xx-utils.git && cd nvidia-39
 #(wget https://raw.githubusercontent.com/simurgh314/Arch-GPT-/main/6_video.sh && sudo chmod +x 6_video.sh && sh 6_video.sh)
 
 cd && git clone https://aur.archlinux.org/nvidia-390xx.git && cd nvidia-390xx && makepkg -sri
+
+exit
