6ece413dfedbd6a0338e285a7d3c174bd65f53f2
Simurgh <62325871+simurgh314@users.noreply.github.com>
2021-03-17 22:37:05 +0500

    Update 6_video.sh

--- 6_video.sh
+++ 6_video.sh
@@ -2,12 +2,12 @@
 
 
 (
+echo 'harahara108';
 echo Y;
-echo y;
 echo ;
-) | git clone https://aur.archlinux.org/nvidia-390xx-utils.git && cd nvidia-390xx-utils && makepkg -sri
+) | cd && git clone https://aur.archlinux.org/nvidia-390xx-utils.git && cd nvidia-390xx-utils && makepkg -sri
 
-(echo Y;
-echo y;
-echo ;
-) | git clone https://aur.archlinux.org/nvidia-390xx.git && cd nvidia-390xx && makepkg -sri
+#(echo Y;
+#echo y;
+#echo ;
+#) | cd && git clone https://aur.archlinux.org/nvidia-390xx.git && cd nvidia-390xx && makepkg -sri
