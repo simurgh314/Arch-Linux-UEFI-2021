7a95bb8d07bfffbefe8bad36d4c3f6090741725c
Simurgh <62325871+simurgh314@users.noreply.github.com>
2021-03-17 23:17:06 +0500

    Update 5_pakety.sh

--- 5_pakety.sh
+++ 5_pakety.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+echo 'УСТАНОВКА Xorg'
+pacman -Syu --noconfirm xorg-server xorg-xinit xorg-apps xterm mesa-libgl &&
 pacman -S --noconfirm ttf-font-awesome ttf-liberation ttf-dejavu opendesktop-fonts ttf-bitstream-vera ttf-arphic-uming ttf-hanazono ttf-arphic-ukai &&
-pacman -S --noconfirm i3-gaps rofi i3status nitrogen flameshot xfce4-clipman-plugin lxappearance tumbler numlockx ristretto unzip kolourpaint usbutils ufw speedcrunch gpart gparted catfish neofetch htop xfce4-terminal gvfs 
+pacman -S --noconfirm i3-gaps rofi i3status nitrogen flameshot xfce4-clipman-plugin lxappearance tumbler numlockx firefox ristretto unzip kolourpaint usbutils ufw speedcrunch gpart gparted catfish neofetch htop xfce4-terminal gvfs &&  
+pacman -S alsa alsa-plugins alsa-utils pulseaudio pulseaudio-equalizer pulseaudio-jack pavucontrol pasystray
 
 
