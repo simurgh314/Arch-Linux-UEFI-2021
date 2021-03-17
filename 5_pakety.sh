b4534c780e9aef5e1264dc7e5e0aa2f491b55b6f
Simurgh <62325871+simurgh314@users.noreply.github.com>
2021-03-17 23:17:46 +0500

    Update 5_pakety.sh

--- 5_pakety.sh
+++ 5_pakety.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'УСТАНОВКА Xorg'
+echo 'УСТАНОВКА ПРОГРАММ'
 pacman -Syu --noconfirm xorg-server xorg-xinit xorg-apps xterm mesa-libgl &&
 pacman -S --noconfirm ttf-font-awesome ttf-liberation ttf-dejavu opendesktop-fonts ttf-bitstream-vera ttf-arphic-uming ttf-hanazono ttf-arphic-ukai &&
 pacman -S --noconfirm i3-gaps rofi i3status nitrogen flameshot xfce4-clipman-plugin lxappearance tumbler numlockx firefox ristretto unzip kolourpaint usbutils ufw speedcrunch gpart gparted catfish neofetch htop xfce4-terminal gvfs &&  
