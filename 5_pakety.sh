9fed3af4fff1656bc604b2a64466075133703c96
Simurgh <62325871+simurgh314@users.noreply.github.com>
2021-03-17 23:24:25 +0500

    Update 5_pakety.sh

--- 5_pakety.sh
+++ 5_pakety.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 echo 'УСТАНОВКА ПРОГРАММ'
-pacman -Syu --noconfirm xorg-server xorg-xinit xorg-apps xterm mesa-libgl &&
-pacman -S --noconfirm ttf-font-awesome ttf-liberation ttf-dejavu opendesktop-fonts ttf-bitstream-vera ttf-arphic-uming ttf-hanazono ttf-arphic-ukai &&
-pacman -S --noconfirm i3-gaps rofi i3status nitrogen flameshot xfce4-clipman-plugin lxappearance tumbler numlockx firefox ristretto unzip kolourpaint usbutils ufw speedcrunch gpart gparted catfish neofetch htop xfce4-terminal gvfs &&  
-pacman -S alsa alsa-plugins alsa-utils pulseaudio pulseaudio-equalizer pulseaudio-jack pavucontrol pasystray
+sudo pacman -Syu --noconfirm xorg-server xorg-xinit xorg-apps xterm mesa-libgl ttf-font-awesome ttf-liberation ttf-dejavu opendesktop-fonts ttf-bitstream-vera ttf-arphic-uming ttf-hanazono ttf-arphic-ukai i3-gaps rofi i3status nitrogen flameshot xfce4-clipman-plugin lxappearance tumbler numlockx firefox ristretto unzip kolourpaint usbutils ufw speedcrunch gpart gparted catfish neofetch htop xfce4-terminal gvfs alsa alsa-plugins alsa-utils pulseaudio pulseaudio-equalizer pulseaudio-jack pavucontrol pasystray
 
 
