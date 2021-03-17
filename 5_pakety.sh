d90b21116b286a613bb22e1e6526a6476a18a46a
Simurgh <62325871+simurgh314@users.noreply.github.com>
2021-03-17 23:08:05 +0500

    Create 5_pakety.sh

---
+++ 5_pakety.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pacman -S --noconfirm ttf-font-awesome ttf-liberation ttf-dejavu opendesktop-fonts ttf-bitstream-vera ttf-arphic-uming ttf-hanazono ttf-arphic-ukai &&
+pacman -S --noconfirm i3-gaps rofi i3status nitrogen flameshot xfce4-clipman-plugin lxappearance tumbler numlockx ristretto unzip kolourpaint usbutils ufw speedcrunch gpart gparted catfish neofetch htop xfce4-terminal gvfs 
+
+
