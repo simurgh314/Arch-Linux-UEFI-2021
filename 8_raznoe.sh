00793df190f6378cbf5e152fb5423200ae2812b7
Simurgh <62325871+simurgh314@users.noreply.github.com>
2021-03-18 18:14:40 +0500

    Update 8_raznoe.sh

--- 8_raznoe.sh
+++ 8_raznoe.sh
@@ -14,3 +14,18 @@ echo '
 2) sudo netctl enable wlp2s0-matrix777
 В случае проводного, у вас должен быть активен интрефейс (sudo ip link set nazvanie_interfeysa up)
 и запущен dhcp сервис (sudo systemctl start dhcpcd) (ну или настроены адреса вручную)
+
+##########################################################################################
+
+cp /etc/X11/xinit/xinitrc ~/.xinitrc &&
+echo '[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx' >> .bash_profile &&
+
+
+
+sudo mkdir -p /etc/systemd/system/getty@tty1.service.d/ 
+sudo touch /etc/systemd/system/getty@tty1.service.d/override.conf
+sudo echo '
+[Service]
+ExecStart=
+ExecStart=-/usr/bin/agetty --autologin $USER --noclear %I $TERM' > nano /etc/systemd/system/getty@tty1.service.d/override.conf
+#проверить
