48983e860dd3cadecc1b03917086e8d548f3cd55
Simurgh <62325871+simurgh314@users.noreply.github.com>
2021-03-18 19:36:35 +0500

    Update 5_pakety.sh

--- 5_pakety.sh
+++ 5_pakety.sh
@@ -3,37 +3,9 @@
 cd &&
 echo 'УСТАНОВКА ПРОГРАММ'
 
-pack=(
-xorg-server
-xorg-xinit
-xorg-apps
-xterm
-mesa-libgl
-ttf-font-awesome
-ttf-liberation
-ttf-dejavu
-opendesktop-fonts
-ttf-bitstream-vera
-ttf-arphic-uming
-ttf-hanazono
-ttf-arphic-ukai
-i3-gaps
-i3status
-nitrogen
-xfce4-clipman-plugin
-lxappearance
-tumbler
-numlockx
-firefox
-ristretto
-unzip
-usbutils
-speedcrunch
-neofetch
-xfce4-terminal
-gvfs
-)
+sudo pacman -Syu --noconfirm xorg-server xorg-xinit xorg-apps xterm mesa-libgl ttf-font-awesome ttf-liberation ttf-dejavu opendesktop-fonts ttf-bitstream-vera ttf-arphic-uming ttf-hanazono ttf-arphic-ukai i3-gaps i3status nitrogen xfce4-clipman-plugin lxappearance tumbler numlockx ristretto unzip usbutils speedcrunch neofetch xfce4-terminal gvfs
 
-sudo pacman -Syu --noconfirm $pack
+
+
 #rofi pasystray pavucontrol pulseaudio-jack pulseaudio-equalizer flameshot thunar
-#alsa-plugins alsa-utils pulseaudio alsa gpart gparted kolourpaint ufw htop catfish
+#alsa-plugins alsa-utils pulseaudio alsa gpart gparted kolourpaint ufw htop catfish firefox
